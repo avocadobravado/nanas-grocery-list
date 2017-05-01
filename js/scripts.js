77e6e6d020a418eb11ca5afebc05613e7a451b96
Michael Koceja and Rose Sponseller <michael@>
2017-05-01 15:39:12 -0700

    add sort to display the list in alphabetical order

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,23 @@ $(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     var groceryItems = ["item1", "item2", "item3", "item4", "item5"];
+    var sortItems = groceryItems.sort();
 
      $("#formOne").fadeOut();
      $(".showList").fadeIn();
 
-    var displayItems = groceryItems.map(function(item) {
-      var item = $("input#" + item).val();
-      $("ul").append("<li>" + item.toUpperCase() + "</li>");
+
+    var displayItems = sortItems.map(function(item) {
+      return $("input#" + item).val();
     });
+
+
+    var sortedArray = displayItems.sort();
+
+    sortedArray.forEach(function(nana) {
+      $("ul").append("<li>" + nana.toUpperCase() + "</li>");
+    });
+
   });
 });
     // var displayItems = groceryItems.map(function(item) {
@@ -23,7 +32,6 @@ $(function(){
 // Create a form that allows a user to input items they need at the store.
 // When the user submits the form, hide the form and display the list of items in alphabetical order, all uppercased.
 // Hint: To do this, build an array of the inputted items. (Make sure you don't include any blank fields.) Sort the array in alphabetical order. Make a new array with each entry uppercased (don't just uppercase them when you display them). Then, display the sorted list of things as list items inside a <ul>.
-
 //
 // $(function(){
 //   $("#formOne").submit(function(event){
@@ -33,14 +41,10 @@ $(function(){
 //      $("#formOne").fadeOut();
 //      $(".showList").fadeIn();
 //
-//      groceryItems.forEach(function(item) {
-//        var item = $("input#" + item).val();
-//        $("." + item).text(item);
-//        $("ul").append("<li>" + item + "</li>");
-//      });
-//
-//     // var displayItems = groceryItems.map(function(item) {
-//     //   alert(items.toUpperCase());
-//     // });
+//     var displayItems = groceryItems.map(function(item) {
+//       var item = $("input#" + item).val();
+//       //var sortItems = displayItems.sort();
+//       $("ul").append("<li>" + item.toUpperCase() + "</li>");
+//     });
 //   });
 // });
